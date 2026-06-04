a1dd24dd6d49b363c121171e2f09382d6b6a482a
yogarajan <yogaraja.g@gmail.com>
2026-06-04 21:39:46 +0530

    find sonarqube issue

--- AutomaticBuild/Controllers/WeatherForecastController.cs
+++ AutomaticBuild/Controllers/WeatherForecastController.cs
@@ -19,9 +19,43 @@ namespace AutomaticBuild.Controllers
         }
 
 
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        //[HttpGet("{id}")]
+        //public ActionResult<WeatherForecast> GetById(int id)
+        //{
+        //    if (id < 1 || id > 5)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var forecast = new WeatherForecast
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(id)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    };
+
+        //    return Ok(forecast);
+        //}
+
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            // Unused variable (SonarQube: Remove this unused 'x' local variable)
+            int x = 0;
+
+            // Magic number (SonarQube: Replace this magic number with a named constant)
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -34,19 +68,30 @@ namespace AutomaticBuild.Controllers
         [HttpGet("{id}")]
         public ActionResult<WeatherForecast> GetById(int id)
         {
-            if (id < 1 || id > 5)
+            // Empty catch block (SonarQube: Either log or rethrow this exception)
+            try
             {
-                return NotFound();
+                if (id < 1 || id > 5)
+                {
+                    return NotFound();
+                }
+
+                var forecast = new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(id)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                };
+
+                return Ok(forecast);
+            }
+            catch
+            {
+                // Do nothing
             }
 
-            var forecast = new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(id)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            };
-
-            return Ok(forecast);
+            // Unreachable code (SonarQube: Remove this statement)
+            return null;
         }
 
     }
